3445050e865f0e9e4fc930ed95a426d9d2f7c008
AnnelieseBreiWM <adbrei@email.wm.edu>
2021-01-12 19:52:19 -0500

    Update description, future research

--- timeline.js
+++ timeline.js
@@ -4,6 +4,7 @@ var t1Modal = document.getElementById("t1Modal")
 var t2Modal = document.getElementById("t2Modal")
 var t3Modal = document.getElementById("t3Modal")
 var t4Modal = document.getElementById("t4Modal")
+var t5Modal = document.getElementById("t5Modal")
 
 // Get all buttons that opens the modals
 var essayBtn = document.getElementById("essayBtn");
@@ -11,6 +12,7 @@ var t1Btn = document.getElementById("t1Btn");
 var t2Btn = document.getElementById("t2Btn");
 var t3Btn = document.getElementById("t3Btn");
 var t4Btn = document.getElementById("t4Btn");
+var t5Btn = document.getElementById("t5Btn");
 
 // Get the <span> element that closes the modal
 var essayClose = document.getElementsByClassName("essayClose")[0];
@@ -18,6 +20,7 @@ var t1Close = document.getElementsByClassName("t1Close")[0];
 var t2Close = document.getElementsByClassName("t2Close")[0];
 var t3Close = document.getElementsByClassName("t3Close")[0];
 var t4Close = document.getElementsByClassName("t4Close")[0];
+var t5Close = document.getElementsByClassName("t5Close")[0];
 
 // When the user clicks the button, open the modal 
 essayBtn.onclick = function() {
@@ -34,6 +37,9 @@ t3Btn.onclick = function() {
 }
 t4Btn.onclick = function() {
     t4Modal.style.display = "block";
+}
+t5Btn.onclick = function() {
+  t5Modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -52,6 +58,9 @@ t3Close.onclick = function() {
 t4Close.onclick = function() {
     t4Modal.style.display = "none";
 }
+t5Close.onclick = function() {
+  t5Modal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -69,5 +78,8 @@ window.onclick = function(event) {
   }
   else if (event.target == t4Modal) {
     t4Modal.style.display = "none";
+  }
+  else if (event.target == t5Modal) {
+    t5Modal.style.display = "none";
   }
 }
